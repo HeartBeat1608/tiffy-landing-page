7bd42880ef582c54adf253780ff51ac44182d32b
Heartbeat1608 <heartbeatanna1608@gmail.com>
2023-10-06 17:57:51 +0530

    fix: header line height

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 import { useState } from "react";
 import Modal from "./Modal";
 import mixpanel from "mixpanel-browser";
@@ -7,25 +7,28 @@ const HeroSection = () => {
   const [open, setOpen] = useState(false);
 
   const handleOpen = () => {
-    mixpanel.track("hero:join-button")
-    setOpen(true)
-  }  
+    mixpanel.track("hero:join-button");
+    setOpen(true);
+  };
 
   const handleClose = () => {
-    setOpen(false)
-  }
+    setOpen(false);
+  };
 
   return (
     <div className="bg-white w-full">
       <div className="container mx-auto px-6 md:px-0 flex items-center justify-center py-4 md:py-8 md:gap-20 gap-8">
         <div className="flex flex-col gap-4">
-          <h2 className="font-bold text-3xl md:text-4xl text-black font-poppins leading-snug text-center md:text-left">
+          <h2 className="font-bold text-3xl md:text-4xl text-black font-poppins !leading-snug text-center md:text-left">
             <span className="text-emerald-500">Home Alone</span> And Hungry?
             <br />
             Let&apos;s Serve You Best{" "}
             <span className="text-emerald-500">Tiffins.</span>
           </h2>
-          <button className="font-medium text-lg px-10 py-1 bg-emerald-500 rounded-full w-fit text-white mx-auto mt-4" onClick={handleOpen}>
+          <button
+            className="font-medium text-lg px-10 py-1 bg-emerald-500 rounded-full w-fit text-white mx-auto mt-4"
+            onClick={handleOpen}
+          >
             Join Now
           </button>
         </div>
