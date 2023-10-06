495affef08300135fa004332781514e542f496ab
Heartbeat1608 <heartbeatanna1608@gmail.com>
2023-10-06 17:55:01 +0530

    feat: update card data

--- components/WhyTiffy.tsx
+++ components/WhyTiffy.tsx
@@ -7,37 +7,42 @@ const Card = ({ title, description }: CardProps) => {
       <header className="font-bold text-lg text-center w-full text-emerald-500 py-2">
         {title}
       </header>
-      <div className="font-light text-center border-t-2 border-t-emerald-500 border-dashed pt-3">{description}</div>
+      <div className="font-light text-center border-t-2 border-t-emerald-500 border-dashed pt-3">
+        {description}
+      </div>
     </div>
   );
 };
 
 const cardItems: CardProps[] = [
   {
-    title: "Understanding Business Needs",
+    title: "Localized",
     description:
-      "We understand the business needs of our clients and provide them with the best solutions to meet their needs.",
+      "Tiffy provides you with the best tiffins in your locality. Choose your meal and we will handle the rest.",
   },
   {
-    title: "Experienced Professionals",
+    title: "Variety",
     description:
-      "Our team of experienced professionals provide quality services and products to our clients.",
+      "We provide you with a wide range of tiffins to choose from. You can choose a meal from the various options available.",
   },
   {
-    title: "Customer Satisfaction and Support",
+    title: "Affordable",
     description:
-      "We are committed to deliver the best possible customer experience.",
+      "Get the best quality tiffins at the most affordable prices. We provide you with the best value for your money.",
   },
   {
-    title: "Best Quality Food and Prices",
+    title: "Easy",
     description:
-      "We are committed to deliver the best quality tiffins that will make your day cheerful.",
+      "It is very easy to order tiffins from Tiffy. Just select your meal and we will deliver it to your doorstep.",
   },
 ];
 
 const WhyTiffy = () => {
   return (
-    <div className="bg-white w-full bg-gradient-to-b from-10% from-white via-emerald-300/70 to-white to-90%" id="about">
+    <div
+      className="bg-white w-full bg-gradient-to-b from-10% from-white via-emerald-300/70 to-white to-90%"
+      id="about"
+    >
       <div className="container mx-auto px-6 md:px-0 flex flex-col items-center justify-center py-8 backdrop-blur-xl">
         <h2 className="text-2xl font-bold mb-8">Why Tiffy</h2>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 items-center justify-around gap-6 w-10/12">
